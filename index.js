ef1c6e2ee066303a3af7a861f2374018e87510fb
Angela Tolwani <angela@tolwani.com>
2024-04-08 21:23:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+  function writeCards(listOfNames, eventName){
+    const messageArray = []
+    for (let i = 0; i < listOfNames.length; i++) {
+        messageArray[i] = `Thank you, ${listOfNames[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return messageArray;
+  }
+
+  function countDown(posInteger){
+    let i = posInteger;
+    while (i >= 0){
+        console.log(i);
+        i --;
+    }
+  }
